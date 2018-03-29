f88e9e4ec3790a7e7647e2874bde9565160557b0
shakkazombie <lukaszbabicki@gmail.com>
2018-03-29 23:23:48 +0200

    Update scripts.js
    Zmieniono nazwy zmiennych z dinosaur na dinos

--- js/scripts.js
+++ js/scripts.js
@@ -2,8 +2,8 @@
 
 var text = 'Velociraptor is a genus of herbivorous ceratopsid dinosaur that first appeared during the late Maastrichtian stage of the late Cretaceous period.';
 var dinosaur = 'Triceratops';
-var dinosaur = dinosaur.toUpperCase();
-var text = text.replace("Velociraptor", dinosaur);
+var dinos = dinosaur.toUpperCase();
+var text = text.replace("Velociraptor", dinos);
 
-document.write(dinosaur + "<br/>");
+document.write(dinos + "<br/>");
 document.write(text.substr(0, text.length / 2));
